ec382cd0ea841eba3ba888feea9815239ea8a848
morgan <justmogen@gmail.com>
2025-08-25 14:35:54 +0300

    fixed procfile to use default port as EB

--- pethub/urls.py
+++ pethub/urls.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
+
+def health_check(request):
+    return JsonResponse({"status": "healthy", "message": "PetHub API is running"})
 
 urlpatterns = [
+    path('', health_check, name='health_check'),
     path('admin/', admin.site.urls),
     path('', include('pets.urls')),
 ]
