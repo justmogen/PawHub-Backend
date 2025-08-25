f9e80d69630f0da1f4346119f4198ccc89b00a1d
morgan <justmogen@gmail.com>
2025-08-25 12:58:19 +0300

    fixed the core model for uuid addition

--- core/models.py
+++ core/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+import uuid
 
 
 class TimeStampedModel(models.Model):
     """
     Abstract base model that provides self-updating 
-    'created_at' and 'updated_at' fields.
+    'created_at' and 'updated_at' fields with UUID primary key.
     """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
